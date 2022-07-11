f8ffa797c13e9b1e4ecaa546696e173197887d90
yuqin hu <90234462+YuqinHu@users.noreply.github.com>
2022-07-11 13:41:54 -0700

    design logou bottun and finish home page

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const { Pool } = require('pg');
 
 var pool;
 pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    connectionString: 'postgres://postgres:Hyq2033221722a@localhost/users',
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
 })
 const PORT = process.env.PORT || 5000
 
@@ -58,11 +58,9 @@ app.post('/create_account', (req, res) => {
 })
 
 app.get('/table', (req, res) => {
-    if (req.session.uid)
-    {
-        if(req.session.type == "admin" || req.session.type == "superadmin")
-        {
-            pool.query('SELECT * From usr where type = $1 OR type = $2',['user','admin'],(error, result) => {
+    if (req.session.uid) {
+        if (req.session.type == "admin" || req.session.type == "superadmin") {
+            pool.query('SELECT * From usr where type = $1 OR type = $2', ['user', 'admin'], (error, result) => {
                 if (error) {
                     res.send(error);
                 }
@@ -70,44 +68,44 @@ app.get('/table', (req, res) => {
                 res.render('pages/table', results)
             })
         }
-        else{
+        else {
             req.session.destroy();
             res.redirect("/");
         }
     }
-    else{
+    else {
         res.redirect("/");
     }
 
 })
 
 app.post('/delete/:id', function (req, res) {
-    if(req.session.uid){
-            const uid = parseInt(req.params['id']);
-            pool.query(`DELETE FROM usr WHERE uid=$1`, [uid], function (error, result) {
-                if (error) {
-                    res.send(error);
-                }
-                if(req.session.uid == uid){
-                    pool.query(`DELETE FROM session_comment WHERE uid=$1`, [uid], function (error, result) {
+    if (req.session.uid) {
+        const uid = parseInt(req.params['id']);
+        pool.query(`DELETE FROM usr WHERE uid=$1`, [uid], function (error, result) {
+            if (error) {
+                res.send(error);
+            }
+            if (req.session.uid == uid) {
+                pool.query(`DELETE FROM session_comment WHERE uid=$1`, [uid], function (error, result) {
                     if (error) {
                         res.send(error);
                     }
-                    })
-                    req.session.destroy();
-                    res.redirect('/');   
-                }
-                else{
-                    pool.query(`DELETE FROM session_comment WHERE uid=$1`, [uid], function (error, result) {
-                        if (error) {
-                            res.send(error);
-                        }
-                        res.redirect('/table');
-                    })
-                }
+                })
+                req.session.destroy();
+                res.redirect('/');
+            }
+            else {
+                pool.query(`DELETE FROM session_comment WHERE uid=$1`, [uid], function (error, result) {
+                    if (error) {
+                        res.send(error);
+                    }
+                    res.redirect('/table');
+                })
+            }
         })
     }
-    else{
+    else {
         res.redirect('/');
     }
 });
@@ -120,14 +118,15 @@ app.post('/auth', (req, res) => {
             if (error) {
                 res.send('error');
             }
+
             if (result.rows.length > 0) {
                 if (result.rows[0]['type'] == "user") {
                     req.session.username = result.rows[0]['name'];
                     req.session.uid = result.rows[0]["uid"];
                     req.session.type = result.rows[0]['type'];
-                    res.redirect('/video');
+                    res.redirect('/home.html');
                 }
-                if (result.rows[0]['type'] == "admin"  || result.rows[0]['type']  == "superadmin"){
+                if (result.rows[0]['type'] == "admin" || result.rows[0]['type'] == "superadmin") {
                     req.session.username = result.rows[0]['name'];
                     req.session.uid = result.rows[0]["uid"];
                     req.session.type = result.rows[0]['type'];
@@ -144,104 +143,103 @@ app.post('/auth', (req, res) => {
     }
 })
 
-app.get('/view_comments',(req,res) => {
-    if (req.session.uid ){
-        if(req.session.type == "admin" || req.session.type == "superadmin"){
-            pool.query('Select * from  session_comment',(error,result)=>{
+app.get('/view_comments', (req, res) => {
+    if (req.session.uid) {
+        if (req.session.type == "admin" || req.session.type == "superadmin") {
+            pool.query('Select * from  session_comment', (error, result) => {
                 if (error)
-                res.end(error);
-                var result = {'rows':result.rows}
-                res.render('pages/table_comments',result); 
-              })
+                    res.end(error);
+                var result = { 'rows': result.rows }
+                res.render('pages/table_comments', result);
+            })
         }
-        else{
+        else {
             req.session.destroy();
             res.redirect("/");
         }
     }
-    else
-    {
+    else {
         res.redirect("/");
     }
 })
 
 app.get('/add_admin', (req, res) => {
     if (req.session.uid) {
-        if(req.session.type == "admin" || req.session.type == "superadmin"){
+        if (req.session.type == "admin" || req.session.type == "superadmin") {
             res.render('pages/add_admin')
         }
-        else{
+        else {
             req.session.destroy();
             res.redirect("/");
         }
     }
-    else{
+    else {
         res.redirect("/");
     }
 })
 
 app.post('/create_addmin_account', (req, res) => {
-    if(req.session.uid){
+    if (req.session.uid) {
         var usr = req.body
         pool.query('INSERT INTO usr (name, email, type, password) VALUES ($1, $2, $3, $4)',
-        [usr.name, usr.email, 'admin', usr.password], function (error, result) {
-            if (error) {
-                res.render('pages/incorrect')
-            }
-            res.redirect('/table');
-        })
+            [usr.name, usr.email, 'admin', usr.password], function (error, result) {
+                if (error) {
+                    res.render('pages/incorrect')
+                }
+                res.redirect('/table');
+            })
     }
-    else{
+    else {
         res.redirect("/");
     }
 })
 
 
 app.post('/delete_comment/:id', function (req, res) {
-    if(req.session.uid){
+    if (req.session.uid) {
         const uid = parseInt(req.params['id']);
         pool.query(`DELETE FROM session_comment WHERE comment_id=$1`, [uid], function (error, result) {
-        if (error) {
-            res.send(error);
-        }
-        res.redirect('/view_comments');
+            if (error) {
+                res.send(error);
+            }
+            res.redirect('/view_comments');
         })
     }
-    else{
+    else {
         res.redirect("/");
     }
-    
+
 });
 
-app.get('/video',(req,res)=> {
+app.get('/video', (req, res) => {
     //if user login and session exist load the page
-    if (req.session.uid)
-    {
-        if(req.session.type == "admin" || req.session.type == "superadmin"){
+    if (req.session.uid) {
+        if (req.session.type == "admin" || req.session.type == "superadmin") {
             var getCommentQuery = 'Select * from  session_comment';
         }
-        else{
+        else {
             var getCommentQuery = `Select * from  session_comment WHERE uid = '${req.session.uid}'`;
         }
-        pool.query(getCommentQuery,(error,result)=>{
-        if (error)
-        res.end(error);
-        var result = {'rows':result.rows}
-        res.render('pages/video',result); 
-    })}
+        pool.query(getCommentQuery, (error, result) => {
+            if (error)
+                res.end(error);
+            var result = { 'rows': result.rows }
+            res.render('pages/video', result);
+        })
+    }
     //if user not login, rediect to the main page
-    else{
+    else {
         res.redirect("/");
     }
-  })
+})
 
-app.get('/logout',(req,res) => {
+app.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 })
 
-app.post('/addComment',(req,res)=>{
-    if(req.session.uid){
+app.post('/addComment', (req, res) => {
+    if (req.session.uid) {
         //console.log("post: Add Comment");
         var uid = req.session.uid;
         var name = req.session.username;
@@ -254,40 +252,40 @@ app.post('/addComment',(req,res)=>{
         // current date
         // adjust 0 before single digit date
         let date = ("0" + date_ob.getDate()).slice(-2);
-    
+
         // current month
         let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    
+
         // current year
         let year = date_ob.getFullYear();
-    
+
         // current hours
         let hours = date_ob.getHours();
-    
+
         // current minutes
         let minutes = date_ob.getMinutes();
-    
+
         // current seconds
         let seconds = date_ob.getSeconds();
 
 
         //console.log(timestamp);
-    
+
         var addComment = `INSERT INTO session_comment (name, uid, year, month, date, hours, minutes, seconds, comment) VALUES ('${name}','${uid}','${year}','${month}','${date}','${hours}','${minutes}','${seconds}','${comment}')`;
 
-        pool.query(addComment, (error,results)=>{
-        if (error){
-            res.end(error);
-        }
-        else{
-            res.redirect("/video");
-        }
-    })   
+        pool.query(addComment, (error, results) => {
+            if (error) {
+                res.end(error);
+            }
+            else {
+                res.redirect("/video");
+            }
+        })
     }
-    else{
+    else {
         res.redirect("/");
     }
-    
+
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
